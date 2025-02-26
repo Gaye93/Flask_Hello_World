4bc014b3260efee4a977f640ddf4cf59509300b6
Gaye Camara <122286715+Gaye93@users.noreply.github.com>
2025-02-26 14:01:05 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -29,6 +29,10 @@ def exo2():
 def exo3():
     return render_template('exercice3.html')
 
+@app.route("/formulaire/")
+def form():
+    return render_template('formulaire.html')
+
 @app.route("/cnam/")
 def cnam():
     return render_template('cnam.html')
